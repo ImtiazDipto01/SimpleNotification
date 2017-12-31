0eb092e8eae57153f0156e1030c07a8bba8fd38e
imtiaz <programmer.imtiaz@gmail.com>
2017-12-31 23:51:05 +0600

    ADD- notification send button added !

--- app/src/main/java/com/example/dipto/simplenotification/MainActivity.java
+++ app/src/main/java/com/example/dipto/simplenotification/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.dipto.simplenotification;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonNotification ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonNotification = findViewById(R.id.notify_btn) ;
+        buttonNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
