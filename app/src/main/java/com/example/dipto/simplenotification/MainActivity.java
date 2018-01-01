2be2e4d569c3797708c543d5f79640bcd92ee97f
imtiazdipto <programmer.imtiaz@gmail.com>
2018-01-01 11:00:39 +0600

    CREATE- TestActivity and activity_test.xml ADD- Added notification onclick !!

--- app/src/main/java/com/example/dipto/simplenotification/MainActivity.java
+++ app/src/main/java/com/example/dipto/simplenotification/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.dipto.simplenotification;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +13,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button buttonNotification ;
+    int count = 0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +23,25 @@ public class MainActivity extends AppCompatActivity {
         buttonNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0 /* Request code */, intent,
+                        PendingIntent.FLAG_ONE_SHOT);
 
+                NotificationCompat.Builder notificationBuilder =
+                        (NotificationCompat.Builder) new NotificationCompat.Builder(MainActivity.this)
+                                .setSmallIcon(R.mipmap.ic_launcher)
+                                .setContentTitle("Its Your day!!")
+                                .setContentText("you have awesome notification")
+                                .setAutoCancel(true)
+                                .setContentIntent(pendingIntent);
+
+                NotificationManager notificationManager =
+                        (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                count++ ;
+
+                notificationManager.notify(count, notificationBuilder.build());
             }
         });
     }
